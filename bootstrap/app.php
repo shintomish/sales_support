51fa060f499aa77ded27a3da6882949d32190b02
root <root@v133-18-42-139-vir.kagoya.net>
2026-03-02 16:00:19 +0900

    fix: CSRF除外設定を削除

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,8 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+    	//
     })
+
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
